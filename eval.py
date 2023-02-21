f11f4ef6a52d3a48c4def3a532f3d58177baf7c7
Martin Aumüller <maau@itu.dk>
2023-02-21 14:57:05 +0100

    Allow attributes to be stored as datasets

--- eval.py
+++ eval.py
@@ -46,6 +46,15 @@ def get_recall(I, gt, k):
         recall += len(set(I[i]) & set(gt[i, :k]))
     return recall / (n * k)
 
+def return_h5_str(f, param):
+    if param not in f:
+        return 0
+    x = f[param][()]
+    if type(x) == np.bytes_:
+        return x.decode()
+    return x
+
+
 if __name__ == "__main__":
     true_I_cache = {}
 
@@ -55,11 +64,15 @@ if __name__ == "__main__":
         writer = csv.DictWriter(csvfile, fieldnames=columns)
         writer.writeheader()
         for res in get_all_results("result"):
-            size = res.attrs["size"]
+            try:
+                size = res.attrs["size"]
+                d = dict(res.attrs)
+            except: 
+                size = res["size"][()].decode()
+                d = {k: return_h5_str(res, k) for k in columns}
             if size not in true_I_cache:
                 true_I_cache[size] = get_groundtruth(size)
             recall = get_recall(np.array(res["knns"]), true_I_cache[size], 10)
-            d = dict(res.attrs)
             d['recall'] = recall
             print(d["data"], d["algo"], d["params"], "=>", recall)
             writer.writerow(d)
